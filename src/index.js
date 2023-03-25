d46be8cc27fee3e13d93f8db11d409fa731374fa
Thebrefff <filip_zielinski@o2.pl>
2023-03-25 13:40:13 +0100

    25

--- src/index.js
+++ src/index.js
@@ -5,93 +5,69 @@ import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
+const countriesBox = document.querySelector('#search-box');
+const countriesList = document.querySelector('.country-list');
+const countriesData = document.querySelector('.country-info');
 
+const cleanMarkup = ref => (ref.innerHTML = '');
 
+const inputHandler = e => {
+  const textInput = e.target.value.trim();
 
+  if (!textInput) {
+    cleanMarkup(countriesList);
+    cleanMarkup(countriesData);
+    return;
+  }
 
+  fetchCountries(textInput)
+    .then(data => {
+      console.log(data);
+      if (data.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      }
+      renderMarkup(data);
+    })
+    .catch(error => {
+      cleanMarkup(countriesList);
+      cleanMarkup(countriesData);
+      Notify.failure('Oops, there is no country with that name');
+    });
+};
 
+const renderMarkup = data => {
+  if (data.length < 2) {
+    cleanMarkup(countriesList);
+    const showInfo = createInfo(data);
+    countriesData.innerHTML = showInfo;
+  } else {
+    cleanMarkup(countriesData);
+    const showList = createList(data);
+    countriesList.innerHTML = showList;
+  }
+};
 
+const createList = data => {
+  return data
+    .map(
+      ({ name, flags }) =>
+        `<li><img src="${flags.svg}" alt="${name.official} width="60" height="40">${name.official}</li>`
+    )
+    .join('');
+};
+const createInfo = data => {
+  return data.map(
+    ({ name, capital, population, flags, languages }) =>
+      `<img src="${flags.svg}" alt="${name.official} width="200" height="100">
+      <h1>${name.official}</h1>
+      <p>Capital: ${capital}</p>
+      <p>Population: ${population}</p>
+      <p>Languages: ${Object.values(languages)}</p> `
+  );
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const searchEl = document.querySelector('#search-box');
-// const countryInfo = document.querySelector('.country-info');
-// const countryList = document.querySelector('.country-list');
-
-// const cleanMarkup = ref => (ref.innerHTML = '');
-
-// const inputHandler = e => {
-//   const textInput = e.target.value.trim();
-
-//   if (!textInput) {
-//     cleanMarkup(countryList);
-//     cleanMarkup(countryInfo);
-//     return;
-//   }
-
-//   fetchCountries(textInput)
-//     .then(data => {
-//       console.log(data);
-//       if (data.length > 10) {
-//         Notify.info(
-//           'Too many matches found. Please enter a more specific name'
-//         );
-//         return;
-//       }
-//       renderMarkup(data);
-//     })
-//     .catch(err => {
-//       cleanMarkup(countryList);
-//       cleanMarkup(countryInfo);
-//       Notify.failure('Oops, there is no country with that name');
-//     });
-// };
-
-// const renderMarkup = data => {
-//   if (data.length === 1) {
-//     cleanMarkup(countryList);
-//     const markupInfo = createInfoMarkup(data);
-//     countryInfo.innerHTML = markupInfo;
-//   } else {
-//     cleanMarkup(countryInfo);
-//     const markupList = createListMarkup(data);
-//     countryList.innerHTML = markupList;
-//   }
-// };
-
-// const createListMarkup = data => {
-//   return data
-//     .map(
-//       ({ name, flags }) =>
-//         `<li><img src="${flags.png}" alt="${name.official}" width="60" height="40">${name.official}</li>`
-//     )
-//     .join('');
-// };
-
-// const createInfoMarkup = data => {
-//   return data.map(
-//     ({ name, capital, population, flags, languages }) =>
-//       `<img src="${flags.png}" alt="${name.official}" width="200" height="100">
-//       <h1>${name.official}</h1>
-//       <p>Capital: ${capital}</p>
-//       <p>Population: ${population}</p>
-//       <p>Languages: ${Object.values(languages)}</p>`
-//   );
-// };
-
-// searchEl.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
+countriesList.style.listStyleType = 'none';
+countriesBox.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
